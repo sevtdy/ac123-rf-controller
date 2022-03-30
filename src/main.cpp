5510235824251380ce6e58d74f2e768418e95052
dy.li <dy.li@aftership.com>
2022-03-30 19:00:38 +0800

    support more controller

--- src/main.cpp
+++ src/main.cpp
@@ -7,44 +7,65 @@ const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWD;
 ESP8266WebServer server(80);
 
-const byte REMOTE_BYTE_1 = 0b10100011;
-const byte REMOTE_BYTE_2 = 0b00101001;
-const byte REMOTE_BYTE_3 = 0b10110000;
-const byte REMOTE_BYTE_4 = 0b00101101;
-const byte CHANNEL_PADDING = 0b00000000;
+const unsigned long REMOTE_6_14 = 0xa329b02d;
+const unsigned long REMOTE_6_15 = 0xa3507273;
 
+const byte CHANNEL_PADDING = 0b00000000;
 const byte UP = 0b00001011;
 const byte STOP = 0b00100011;
 const byte DOWN = 0b01000011;
 static const RCSwitch::Protocol customprotocol = {15, 132, 50, {105, 13}, {3, 13}, {11, 5}, {11, 17}, false};
 RCSwitch rfTransmitter = RCSwitch();
 
-void sendRfSignal(byte channel, byte action)
+void sendRfSignal(unsigned long deviceId, byte channel, byte action)
 {
-  byte checksum = REMOTE_BYTE_2 + REMOTE_BYTE_3 + REMOTE_BYTE_4 + channel + CHANNEL_PADDING + action;
-  byte arr[] = {REMOTE_BYTE_1, REMOTE_BYTE_2, REMOTE_BYTE_3, REMOTE_BYTE_4, channel, CHANNEL_PADDING, action, checksum};
-  char sendCodeChar[64];
+  byte idByte1 = deviceId >> 24;
+  byte idByte2 = deviceId >> 16;
+  byte idbyte3 = deviceId >> 8;
+  byte idByte4 = deviceId;
+  byte checksum = idByte2 + idbyte3 + idByte4 + channel + CHANNEL_PADDING + action;
+  byte arr[] = {idByte1, idByte2, idbyte3, idByte4, channel, CHANNEL_PADDING, action, checksum};
+  char code[64];
   for (int i = 0; i < 8; i++)
   {
     for (int j = 0; j < 8; j++)
     {
       if (bitRead(arr[i], 7 - j))
       {
-        sendCodeChar[i * 8 + j] = '1';
+        code[i * 8 + j] = '1';
       }
       else
       {
-        sendCodeChar[i * 8 + j] = '0';
+        code[i * 8 + j] = '0';
       }
     }
   }
-  rfTransmitter.send(sendCodeChar);
-  Serial.printf("sended code: %s \n", sendCodeChar);
-};
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+  }
+  rfTransmitter.send(code);
+  Serial.printf("sended code: %s \n", code);
+}
+
 void handleRoot()
 {
   server.send(200, "text/plain", "hello from esp8266!\r\n");
 }
+
+void redirectToNewPath()
+{
+  String channel = server.pathArg(0);
+  String action = server.pathArg(1);
+  String newPath = "/floor/6/remote/14/channel/" + channel + "/" + action;
+  Serial.printf("redirect to: %s \n", newPath.c_str());
+  server.sendHeader("Location", newPath, true);
+  server.send(301, "text/plain", "");
+}
+
 void handleNotFound()
 {
   String message = "File Not Found\n\n";
@@ -61,32 +82,27 @@ void handleNotFound()
   }
   server.send(404, "text/plain", message);
 }
-void handleAction()
+
+void handleTask()
 {
-  String channel = server.pathArg(0);
-  String action = server.pathArg(1);
-  Serial.printf("channel:%s, action:%s \n", channel.c_str(), action.c_str());
-  int offset = channel.toInt();
-  for (int i = 0; i < 3; i++)
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-  }
+  String remote = server.pathArg(0);
+  String channel = server.pathArg(1);
+  String action = server.pathArg(2);
+  Serial.printf("remote:%s, channel:%s, action:%s \n", remote.c_str(), channel.c_str(), action.c_str());
+  byte channelByte = channel == "all" ? 0b11111111 : 0b1 << (channel.toInt() - 1);
+  unsigned long remoteHex = remote == "14" ? REMOTE_6_14 : REMOTE_6_15;
   if (action == "up")
   {
-
-    sendRfSignal(0b1 << (offset - 1), UP);
+    sendRfSignal(remoteHex, channelByte, UP);
   }
   else if (action == "stop")
   {
 
-    sendRfSignal(0b1 << (offset - 1), STOP);
+    sendRfSignal(remoteHex, channelByte, STOP);
   }
   else
   {
-    sendRfSignal(0b1 << (offset - 1), DOWN);
+    sendRfSignal(remoteHex, channelByte, DOWN);
   }
   server.send(200, "text/plain", "ok");
 }
@@ -121,7 +137,8 @@ void setup()
 
   // setting up server
   server.on("/", handleRoot);
-  server.on(UriRegex("^\\/channel\\/([1-3])\\/(up|stop|down)$"), handleAction);
+  server.on(UriRegex("^\\/channel\\/([1-3])\\/(up|stop|down)$"), redirectToNewPath);
+  server.on(UriRegex("^\\/floor\\/6\\/remote\\/(14|15)\\/channel\\/([1-3]|all)\\/(up|stop|down)$"), handleTask);
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.print("HTTP server started. Runing on: http://");
